70c29bc42f2a237984be35cc04e6852d4571686c
Stephen Matheis <stephen.a.matheis@gmail.com>
2022-06-19 01:41:20 -0500

    Update cities.js

--- netlify/functions/cities.js
+++ netlify/functions/cities.js
@@ -2,48 +2,46 @@ exports.handler = async function (event, context) {
     return {
         statusCode: 200,
         body: JSON.stringify(
-            {
-                "cities": [
-                    {
-                        "section": "cupertino",
-                        "label": "Cupertino",
-                        "area": "America",
-                        "api": "Los_Angeles"
-                    },
-                    {
-                        "section": "new-york-city",
-                        "label": "New York City",
-                        "area": "America",
-                        "api": "New_York"
-                    },
-                    {
-                        "section": "london",
-                        "label": "London",
-                        "area": "Europe"
-                    },
-                    {
-                        "section": "amsterdam",
-                        "label": "Amsterdam",
-                        "area": "Europe"
-                    },
-                    {
-                        "section": "tokyo",
-                        "label": "Tokyo",
-                        "area": "Asia"
-                    },
-                    {
-                        "section": "hong-kong",
-                        "label": "Hong Kong",
-                        "area": "Asia",
-                        "api": "Hong_Kong"
-                    },
-                    {
-                        "section": "sydney",
-                        "label": "Sydney",
-                        "area": "Australia"
-                    }
-                ]
-            } 
+            [
+                {
+                    "section": "cupertino",
+                    "label": "Cupertino",
+                    "area": "America",
+                    "api": "Los_Angeles"
+                },
+                {
+                    "section": "new-york-city",
+                    "label": "New York City",
+                    "area": "America",
+                    "api": "New_York"
+                },
+                {
+                    "section": "london",
+                    "label": "London",
+                    "area": "Europe"
+                },
+                {
+                    "section": "amsterdam",
+                    "label": "Amsterdam",
+                    "area": "Europe"
+                },
+                {
+                    "section": "tokyo",
+                    "label": "Tokyo",
+                    "area": "Asia"
+                },
+                {
+                    "section": "hong-kong",
+                    "label": "Hong Kong",
+                    "area": "Asia",
+                    "api": "Hong_Kong"
+                },
+                {
+                    "section": "sydney",
+                    "label": "Sydney",
+                    "area": "Australia"
+                }
+            ]
         ),
     };
 }
